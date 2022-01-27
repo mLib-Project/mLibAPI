2cb4b21226e86f08fa3e9db82fe89e45e3d2e387
Mateusz <mateuszslotwinski16@gmail.com>
2022-01-27 12:37:50 +0100

    tidying up

--- src/router/router.go
+++ src/router/router.go
@@ -13,13 +13,11 @@ func CreateServer() *gin.Engine {
 	R := gin.Default()
 	R.Use(cors.Default())
 
-	R.LoadHTMLGlob("src/static/*.html")
-	R.Use(static.Serve("/", static.LocalFile("src/static/", false)))
+	R.LoadHTMLGlob("public/*.html")
+	R.Use(static.Serve("/", static.LocalFile("public/", false)))
 	R.NoRoute(Controllers.ServeIndex)
 
-	// MIDDLEWARES
 	R.SetTrustedProxies([]string{"192.168.1.2"})
-
 	R.GET("/api", Controllers.GetBooks)
 
 	return R
